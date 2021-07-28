43f8a5a46d0a7ef5b03aed7f5f47fee4701abdc4
jongeunShin95 <jongeunshin95@gmail.com>
2021-07-29 00:23:56 +0900

    Update db 연결 및 email을 통해 중복 확인, 없다면 db에 저장

---
+++ server/config/dbconfig.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require('mongodb');
+const url = "mongodb://localhost:27017";
+
+const client = new Promise((resolve, reject) => {
+    MongoClient.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, (err, client) => {
+        if (err) reject(err);
+        else resolve(client);
+    });
+});
+
+module.exports = client;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const client = require('../config/dbconfig');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signin', function(req, res, next) {
-  console.log(req.body);
+router.post('/signin', async (req, res, next) => {
+  console.log(req.body.profileObj.email);
+
+  const db = (await client).db('social_login');
+  const collection = db.collection('Users');
+  const result = await collection.findOne({ email: req.body.profileObj.email }); // 해당 email로 db search
+  
+  if (result == null) await collection.insertOne(req.body.profileObj); // 없다면 db에 저장
+
   res.json(req.body);
 });
 
